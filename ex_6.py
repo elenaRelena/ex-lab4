b39b76617a2f0a24c33b5f898c8e4d86f0855e67
elenaRelena <helenessa96@gmail.com>
2016-10-21 18:27:45 +0300

    Add files via upload

--- ex_6.py
+++ ex_6.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import json
 import sys
+import os.path
 from librip.ctxmngrs import timer
 from librip.decorators import print_result
-from librip.gens import field, gen_random
+from librip.gen import field, gen_random
 from librip.iterators import Unique as unique
 
-path = None
+path = path = "data_light.json"
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
 
-with open(path) as f:
+with open(path, encoding="utf8") as f:
     data = json.load(f)
+    #print(type(data))
 
 
 # Далее необходимо реализовать все функции по заданию, заменив `raise NotImplemented`
@@ -23,22 +25,25 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return sorted(unique(field(arg, 'job-name'), ignore_case=True), key=lambda x: x.lower())
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    #return list(filter(lambda x: 'программист' in str(x),arg)) #все, что содержит 'программист'
+    return list(filter(lambda x: str(x).startswith('Программист'),arg)) #все, что начинается с 'программист'
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda x: str(x)+' с опытом Python', arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    res = gen_random(100000, 200000, len(arg))
+    #return list(map(lambda x: x+', зарплата '+str(next(res)) + ' руб.', arg))
+    return list(x+', зарплата '+str(next(res)) + ' руб.' for x in arg)
 
 
 with timer():
